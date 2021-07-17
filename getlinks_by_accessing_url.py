bc6a8a573ea0176daf8665bb07998354c407a3bf
Linuxkay <sccck000@gmail.com>
2021-07-17 21:33:02 +0900

    Added a function extracting links from JavaSript based popup window on get_link_by_accessing_url.py

--- getlinks_by_accessing_url.py
+++ getlinks_by_accessing_url.py
@@ -3,33 +3,39 @@
 from selenium import webdriver
 import time
 import numpy as np
-#import requests
 from bs4 import BeautifulSoup
 import io
 import re
 
 
 browser = webdriver.Chrome()
+# set target url.
 browser.get("https://www.catalog.update.microsoft.com/Search.aspx?q=KB4598457")
-
+# set xpath
 elem = browser.find_element_by_xpath('//*[@id="151315e6-b978-42fc-b0ce-6e5c78a69324"]')
+
 # before clicking button to open popup, store the current window handle
 main_window = browser.current_window_handle
 
 # click whatever button it is to open popup
-
+elem.click()
+# add some sleep if needed. Default value is 0.2sec
+time.sleep(0.2)
+allHandles = browser.window_handles
+# print id of window handle 1
+print(allHandles[0])
+# print id of window handle 2
+print(allHandles[1])
 # after opening popup, change window handle
 for handle in browser.window_handles:
     if handle != main_window:
         popup = handle
-        browser.switch_to_window(popup)
-
+        # change window to handle2
+        browser.switch_to.window(allHandles[1])
 
 print(browser.title) # Should now be the popup window
 
-#elem.click()
-#time.sleep(0.2)
-
+# search href in loop
 elems = browser.find_elements_by_xpath("//a[@href]")
 for elem in elems:
 # show main page html
