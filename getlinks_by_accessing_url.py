7fe350cb0a397170c330a4071f2668347371f19f
Linuxkay <sccck000@gmail.com>
2021-07-18 07:54:47 +0900

    underdev getlinks_by_accessing_url.py

--- getlinks_by_accessing_url.py
+++ getlinks_by_accessing_url.py
@@ -10,8 +10,14 @@ import re
 browser = webdriver.Chrome()
 # set target url.
 browser.get("https://www.catalog.update.microsoft.com/Search.aspx?q=KB4598457")
-# set xpath
-elem = browser.find_element_by_xpath('//*[@id="151315e6-b978-42fc-b0ce-6e5c78a69324"]')
+# set static xpath
+#elem = browser.find_element_by_xpath('//*[@id="151315e6-b978-42fc-b0ce-6e5c78a69324"]')
+
+# set dynamic xpath
+#elem = browser.find_element_by_xpath("//tr[td[contains(text(),'2021-06')] and td[contains(text(),'Download')]]/input").click()
+elem = browser.find_element_by_class_name("resultsbottomBorder.resultspadding") and browser.find_element_by_name('2021-06 .NET 5.0.7 Security Update for x64 Client')
+
+print(elem)
 
 # before clicking button to open popup, store the current window handle
 main_window = browser.current_window_handle
